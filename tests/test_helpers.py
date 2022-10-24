9c12a3b503ebd6e0a306dc087cdef0b6c605f600
nkct <miloszzegota@gmail.com>
2022-10-25 00:04:10 +0200

    outlined the contents of test_helpers.py

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,4 +1,18 @@
 import unittest
 
 class TestHelpers(unittest.TestCase):
-    pass
+    
+    def test_clear(self):
+        pass
+
+    def test_available_properties(self):
+        pass
+
+    def test_indices(self):
+        pass
+
+    def test_new_index(self):
+        pass
+
+    def test_delete_index(self):
+        pass
