730f7b4d70b067a08de25a546ec231e45abb623e
nkct <miloszzegota@gmail.com>
2022-11-09 22:02:15 +0100

    need to fix fetch before any further work

--- src/filter.py
+++ src/filter.py
@@ -13,11 +13,20 @@ def filter(symbols, filters):
                 lval = utils.alias(filter[0])
                 rval = utils.alias(filter[2])
 
-                lval = float(fetch([symbol], [lval])[lval][0])
+                lval_fetch = fetch([symbol], [lval])[lval]
+                rval_fetch = fetch([symbol], [rval])[rval]
+
+                lval = float(lval) if lval.isnumeric() else lval
+                rval = float(rval) if rval.isnumeric() else rval
+
+                lval = lval_fetch[0] if not lval_fetch.empty else lval
+                rval = rval_fetch[0] if not rval_fetch.empty else rval
+                print(fetch(["SYM"], ["0"]))
+
                 op = filter[1]
-                rval = float(filter[2] if filter[2].isnumeric() else fetch([symbol], [rval])[rval][0])
 
-
+                print(lval)
+                print(type(rval))
                 if not utils.eval(lval, op, rval):
                     # is this if even nessecary?
                     if symbol in symbols:
