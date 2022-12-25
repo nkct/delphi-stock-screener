ff744d496f50463af886144cdea84d0cda41ee12
nkct <miloszzegota@gmail.com>
2022-12-25 09:01:25 +0100

    finissed reworking

--- src/filter.py
+++ src/filter.py
@@ -7,29 +7,29 @@ def filter(symbols, filters):
     log.info(f"filter({symbols}, {filters})")
 
     for symbol in symbols[:]:
-            for filter in filters:
-                filter = filter.split()
+        for filter in filters:
+            filter = filter.split()
 
-                lval = utils.alias(filter[0])
-                rval = utils.alias(filter[2])
+            lval = utils.alias(filter[0])
+            rval = utils.alias(filter[2])
 
-                lval_fetch = fetch([symbol], [lval])[lval]
-                rval_fetch = fetch([symbol], [rval])[rval]
+            lval_fetch = fetch([symbol], [lval])[lval]
+            rval_fetch = fetch([symbol], [rval])[rval]
 
-                lval = float(lval) if lval.isnumeric() else lval
-                rval = float(rval) if rval.isnumeric() else rval
+            lval = lval_fetch[0] if not lval_fetch.empty else lval
+            rval = rval_fetch[0] if not rval_fetch.empty else rval
 
-                lval = lval_fetch[0] if not lval_fetch.empty else lval
-                rval = rval_fetch[0] if not rval_fetch.empty else rval
-                print(fetch(["SYM"], ["0"]))
+            lval = str(lval)
+            rval = str(rval)
+                
+            lval = float(lval) if lval.isnumeric() else lval
+            rval = float(rval) if rval.isnumeric() else rval
 
-                op = filter[1]
-
-                print(lval)
-                print(type(rval))
-                if not utils.eval(lval, op, rval):
-                    # is this if even nessecary?
-                    if symbol in symbols:
-                        symbols.remove(symbol)
+            op = filter[1]
+            
+            if not utils.eval(lval, op, rval):
+                # if is nessecary beacouse multiple passes over symbols are made
+                if symbol in symbols:
+                    symbols.remove(symbol)
 
     return symbols
