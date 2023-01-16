780be8ae06de19de3ff8c3544347001c7390d0e5
nkct <miloszzegota@gmail.com>
2023-01-16 12:24:03 +0100

    added testing for clear

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,9 +1,35 @@
 import unittest
+import os
+import sqlite3 as db
+import logging as log
+
+import sys
+sys.path.append('/home/nkct/Documents/projects/python/delphi/')
+
+import src.helpers as helpers
 
 class TestHelpers(unittest.TestCase):
     
+    @classmethod
+    def setUpClass(cls):
+        log.basicConfig(level=log.DEBUG)
+
     def test_clear(self):
-        pass
+        test_db = "test.db"
+        test_table = "test"
+
+        conn = db.connect(test_db)
+        cur = conn.cursor()
+
+        cur.execute(f"CREATE TABLE {test_table} ( test TEXT )")
+
+        helpers.clear(test_db, test_table)
+
+        self.assertTrue(
+            cur.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='{test_table}';")
+        )
+
+        os.remove(test_db)
 
     def test_available_properties(self):
         pass
@@ -16,3 +42,6 @@ class TestHelpers(unittest.TestCase):
 
     def test_delete_index(self):
         pass
+
+if __name__ == '__main__':
+    unittest.main()
